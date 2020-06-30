13e4cfb37557f0bf6b304ff2915f1815bbbc64df
canada87 <ajacassi@gmail.com>
2020-06-30 12:04:19 +0100

    battle algoritmo

---
+++ AJ_lib_battle_environment.py
@@ -0,0 +1,165 @@
+from PIL import Image
+import cv2
+import numpy as np
+
+
+#  ██████   ██████   ██████  ███████ ████████ ████████ ██
+# ██    ██ ██       ██       ██         ██       ██    ██
+# ██    ██ ██   ███ ██   ███ █████      ██       ██    ██
+# ██    ██ ██    ██ ██    ██ ██         ██       ██    ██
+#  ██████   ██████   ██████  ███████    ██       ██    ██
+
+class element:
+    def __init__(self, x, y, x_speed, y_speed, x_size, y_size, w_size, color):
+        self.x = x
+        self.y = y
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+        self.x_size = x_size
+        self.y_size = y_size
+        self.w_size = w_size
+        self.color = color
+
+    def display(self, background):
+        ball_pixels = np.zeros((self.w_size, self.w_size, 3), dtype=np.uint8)
+        ball_pixels[self.x - self.x_size : self.x + self.x_size, self.y - self.y_size : self.y + self.y_size] = self.color
+        background +=ball_pixels
+
+    def action(self, choice):
+        if choice == 0:
+            self.move(y=1)
+        elif choice == 1:
+            self.move(y=-1)
+        elif choice == 2:
+            self.move(y=0)
+
+    def move(self, y):
+        self.y += y
+        if self.y > self.w_size - self.y_size:
+            self.y = self.w_size - self.y_size
+        if self.y < self.y_size:
+            self.y = self.y_size
+
+    def update(self):
+        self.x += self.x_speed
+        self.y += self.y_speed
+
+    def win_lose(self):
+        lose_side = 0
+        if self.x > self.w_size - self.x_size:
+            lose_side = 1
+            return True, lose_side
+        if self.x < self.x_size:
+            lose_side = 2
+            return True, lose_side
+        return False, lose_side
+
+    def boundaries(self):
+        if self.y + self.y_size> self.w_size:
+            self.y = self.w_size - self.y_size
+            self.y_speed *= -1
+
+        if self.y < self.y_size:
+            self.y = self.y_size
+            self.y_speed *= -1
+
+    def hit(self, other):
+        is_hit = 0
+
+        if (np.abs(self.y - other.y) < other.y_size + self.y_size):
+            if (np.abs(other.x + other.x_size - self.x) < self.x_size):
+                if self.x - self.x_size < other.x + other.x_size - 3:
+                    self.x_speed = self.x_speed
+                else:
+                    self.x_speed *= -1
+                    is_hit = 1
+
+        if (np.abs(self.y - other.y) < other.y_size + self.y_size):
+            if (np.abs(other.x - other.x_size - self.x) < self.x_size):
+                if self.x + self.x_size > other.x - other.x_size + 3:
+                    self.x_speed = self.x_speed
+                else:
+                    self.x_speed *= -1
+                    is_hit = 2
+        return is_hit
+
+
+# ███████ ███    ██ ██    ██ ██ ██████   ██████  ███    ██ ███    ███ ███████ ███    ██ ████████
+# ██      ████   ██ ██    ██ ██ ██   ██ ██    ██ ████   ██ ████  ████ ██      ████   ██    ██
+# █████   ██ ██  ██ ██    ██ ██ ██████  ██    ██ ██ ██  ██ ██ ████ ██ █████   ██ ██  ██    ██
+# ██      ██  ██ ██  ██  ██  ██ ██   ██ ██    ██ ██  ██ ██ ██  ██  ██ ██      ██  ██ ██    ██
+# ███████ ██   ████   ████   ██ ██   ██  ██████  ██   ████ ██      ██ ███████ ██   ████    ██
+
+
+class env:
+
+    def __init__(self):
+        self.size = 500
+        self.ball_size = 10
+        self.x_speed = 4
+        self.y_speed = 6
+
+        self.reward_lose_game = -100
+        self.reward_win_game = 100
+        self.reward_hit = 5
+        self.reward_still_alive = -1
+        self.reward_ball_on_eye = 1
+
+    def set_up(self):
+        self.ball = element(self.size//2, self.size//2, self.x_speed, self.y_speed, self.ball_size, self.ball_size, self.size, (255, 175, 0))
+        self.pad1 = element(25, self.size//2, 0, 0, 10, 50, self.size, (0, 255, 0))
+        # self.pad1 = element(25, 273, 0, 0, 10, 50, self.size, (0, 255, 0))
+        self.pad2 = element(self.size-25, self.size//2, 0, 0, 10, 50, self.size, (0, 0, 255))
+
+        observation = self.ball.y, self.ball.x, self.pad1.y, self.pad2.y
+        return np.array(observation)/self.size
+
+    def step(self, action1, action2):
+        self.pad1.action(action1)
+        self.pad2.action(action2)
+
+        self.pad1.boundaries()
+        self.pad2.boundaries()
+        self.ball.boundaries()
+
+        is_hit1 = self.ball.hit(self.pad1)
+        is_hit2 = self.ball.hit(self.pad2)
+
+        self.ball.update()
+
+        done, lose_side = self.ball.win_lose()
+        observation = self.ball.y, self.ball.x, self.pad1.y, self.pad2.y
+
+        def decide_rew(other, is_hit, done, lose_side, numpad):
+            reward = self.reward_still_alive
+            if self.ball.y < other.y + other.y_size and self.ball.y > other.y - other.y_size:
+                reward = self.reward_ball_on_eye
+            if is_hit == 1:
+                reward = self.reward_hit
+            if done:
+                if numpad == 1:
+                    if lose_side == 2:
+                        reward = self.reward_lose_game
+                    if lose_side == 1:
+                        reward = self.reward_win_game
+                else:
+                    if lose_side == 1:
+                        reward = self.reward_lose_game
+                    if lose_side == 2:
+                        reward = self.reward_win_game
+            return reward
+
+        reward1 = decide_rew(self.pad1, is_hit1, done, lose_side, 1)
+        reward2 = decide_rew(self.pad2, is_hit2, done, lose_side, 2)
+
+        return np.array(observation)/self.size, reward1, reward2, done
+
+    def render(self):
+        screen = np.zeros((self.size, self.size, 3), dtype=np.uint8)
+        self.ball.display(screen)
+        self.pad1.display(screen)
+        self.pad2.display(screen)
+
+        img = Image.fromarray(screen, 'RGB')
+        cv2.imshow("image", np.array(img))
+        cv2.waitKey(10)
---
+++ battle_game.py
@@ -0,0 +1,104 @@
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from AJ_lib_battle_environment import env
+from AJ_lib_agent import Agent
+
+
+# ███████ ███████ ███    ██ ███████ ██ ██████  ██      ███████     ██████   █████  ██████   █████  ███    ███ ███████ ████████ ███████ ██████  ███████
+# ██      ██      ████   ██ ██      ██ ██   ██ ██      ██          ██   ██ ██   ██ ██   ██ ██   ██ ████  ████ ██         ██    ██      ██   ██ ██
+# ███████ █████   ██ ██  ██ ███████ ██ ██████  ██      █████       ██████  ███████ ██████  ███████ ██ ████ ██ █████      ██    █████   ██████  ███████
+#      ██ ██      ██  ██ ██      ██ ██ ██   ██ ██      ██          ██      ██   ██ ██   ██ ██   ██ ██  ██  ██ ██         ██    ██      ██   ██      ██
+# ███████ ███████ ██   ████ ███████ ██ ██████  ███████ ███████     ██      ██   ██ ██   ██ ██   ██ ██      ██ ███████    ██    ███████ ██   ██ ███████
+
+# agent
+# load_pre_trained_model = '3x3_999episode__618.00max__513.10avg__105.00min_0.99discount_0.9999epsilondecay_1593110440.model'
+load_pre_trained_model = None
+replay_memory_size = 50_000 #dimensione massima di mosse che possono essere tenute a mente
+sampling_memory = 64 #numenro di mosse usate per il training (sotto campione di replay_memory)
+discount = 0.90
+
+#game
+go_live = True
+save_model = False
+episodes = 20_000 #numero massimo di partite
+sampling_epoc = 100 #ogni quante epoche registrare i risultati
+how_aften_go_live = 5000 #ogni quante epoche fare il live di cosa succede
+how_aften_replace_target = 50 #ogni quanti episodi caricare i pesi da modello live a modello target
+how_aften_train = 4
+min_reward_bar = 1500#valore minimo di reward per salvare i pesi del modello
+model_name = 'pong_battle_8x64x4'
+
+# Exploration settings
+epsilon = 1  # not a constant, going to be decayed
+epsilon_decay = 0.999
+min_epsilon = 0.05
+
+
+# ███    ███  █████  ██ ███    ██
+# ████  ████ ██   ██ ██ ████   ██
+# ██ ████ ██ ███████ ██ ██ ██  ██
+# ██  ██  ██ ██   ██ ██ ██  ██ ██
+# ██      ██ ██   ██ ██ ██   ████
+
+agent1 = Agent(action_space=3, observation_space=4, replay_memory_size=replay_memory_size, epsilon=epsilon,
+                   min_epsilon=min_epsilon, epsilon_decay=epsilon_decay, discount=discount, sampling_memory=sampling_memory,
+                   load_pre_trained_model=load_pre_trained_model)
+agent1.motion_recognition = True
+
+agent2 = Agent(action_space=3, observation_space=4, replay_memory_size=replay_memory_size, epsilon=epsilon,
+                   min_epsilon=min_epsilon, epsilon_decay=epsilon_decay, discount=discount, sampling_memory=sampling_memory,
+                   load_pre_trained_model=load_pre_trained_model)
+agent2.motion_recognition = True
+
+environment = env()
+
+pbar = tqdm(range(1, episodes+1), ascii = True, unit='episode')
+for episode in pbar:
+    episode_reward1 = 0
+    episode_reward2 = 0
+
+    observation = environment.set_up()
+    done = False
+    durata = 0
+
+    ###############################################################################################
+    while not done:
+        action1 = agent1.decide_the_next_action(observation)
+        action2 = agent2.decide_the_next_action(observation)
+
+        next_observation, reward1, reward2, done = environment.step(action1,action2)
+        episode_reward1 += reward1
+        episode_reward2 += reward2
+
+        agent1.update_replay_memory((observation, action1, reward1, next_observation, done))
+        agent2.update_replay_memory((observation, action2, reward2, next_observation, done))
+        observation = next_observation
+
+        agent1.DDQN_train(episode=episode, how_aften_train=how_aften_train, how_aften_replace_target=how_aften_replace_target)
+        agent2.DDQN_train(episode=episode, how_aften_train=how_aften_train, how_aften_replace_target=how_aften_replace_target)
+
+        if go_live and not episode % how_aften_go_live:
+            environment.render()
+        durata += 1
+    ###############################################################################################
+    agent1.update_epsilon()
+    agent2.update_epsilon()
+
+    agent1.save_model(episode_reward1, episode, episodes, sampling_epoc, min_reward_bar, model_name+'agent1', save_model)
+    agent2.save_model(episode_reward2, episode, episodes, sampling_epoc, min_reward_bar, model_name+'agent2', save_model)
+
+    pbar.set_description(f'epsilon {round(agent1.epsilon,3)} reward1 {agent1.aggr_ep_rewards["avg"][-1]} reward2 {agent2.aggr_ep_rewards["avg"][-1]} durata {durata}')
+
+
+plt.plot(agent1.aggr_ep_rewards['ep'], agent1.aggr_ep_rewards['avg'], label='avg1')
+plt.plot(agent1.aggr_ep_rewards['ep'], agent1.aggr_ep_rewards['min'], label='min1')
+plt.plot(agent1.aggr_ep_rewards['ep'], agent1.aggr_ep_rewards['max'], label='max1')
+plt.legend(loc=4)
+plt.show()
+
+plt.plot(agent2.aggr_ep_rewards['ep'], agent2.aggr_ep_rewards['avg'], label='avg2')
+plt.plot(agent2.aggr_ep_rewards['ep'], agent2.aggr_ep_rewards['min'], label='min2')
+plt.plot(agent2.aggr_ep_rewards['ep'], agent2.aggr_ep_rewards['max'], label='max2')
+plt.legend(loc=4)
+plt.show()
